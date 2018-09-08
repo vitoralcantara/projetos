48d348c376db74cc24d2b21f4a5ac2eb6a47f2d3
Vítor Alcântara de Almeida <root@NOTEBOOK-VITOR.localdomain>
2018-09-08 18:41:42 -0300

    Mais coisas feitas

---
+++ inserir_pessoa_DAO.php
@@ -0,0 +1,1 @@
+<?php require_once "conexao_bd.php";
---
+++ sql/create-table-relacoes.sql
@@ -0,0 +1,14 @@
+create table if not exists relacoes (
+    person_id INT not null,
+    child_id INT not null,
+    
+    FOREIGN KEY PERSON (person_id)
+    REFERENCES pessoas(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    
+    FOREIGN KEY CHILD (child_id)
+    REFERENCES pessoas(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+)
